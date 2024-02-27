e09d28d76b83c11740cd0d4585b3852b845cb842
RishiMaersk <rishi.jha@maersk.com>
2024-02-27 10:44:06 +0530

    added builder to switch statement.

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 //import 'package:flutter_application_firebase/services/firebase_options.dart';
 import 'firebase_options.dart';
 
-
-
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
@@ -46,40 +45,51 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: const Text('Register'),
         ),
-        body: Column(
-          children: [
-            TextField(
-              controller: _email,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(hintText: 'Enter your email here'),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: InputDecoration(hintText: 'Enter your password here'),
-            ),
-            TextButton(
-              onPressed: () async {
-                await Firebase.initializeApp(
-                  options: DefaultFirebaseOptions.currentPlatform,
-                );
-
-
-                final email = _email.text;
-                final password = _password.text;
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch(snapshot.connectionState){
               
-                final UserCredential = FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: email, password: password);
-                print(UserCredential);
-              },
-              child: const Text('Register person'),
-            ),
-          ],
+              case ConnectionState.none:
+                // TODO: Handle this case.
+              case ConnectionState.waiting:
+                // TODO: Handle this case.
+            }
+            return Column(
+              children: [
+                TextField(
+                  controller: _email,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration:
+                      InputDecoration(hintText: 'Enter your email here'),
+                ),
+                TextField(
+                  controller: _password,
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration:
+                      InputDecoration(hintText: 'Enter your password here'),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    final email = _email.text;
+                    final password = _password.text;
+
+                    final UserCredential = FirebaseAuth.instance
+                        .createUserWithEmailAndPassword(
+                            email: email, password: password);
+                    print(UserCredential);
+                  },
+                  child: const Text('Register person'),
+                ),
+              ],
+            );
+          },
         ));
   }
 }
